1c1a0f3ebccb60f1e25d6a1c7b25e7d68c7a3dde
Erdi Uçar <ucarerdi@gmail.com>
2019-04-19 21:11:22 +0300

    Initial commit

--- frontend-starting-pack.sh
+++ frontend-starting-pack.sh
@@ -17,31 +17,31 @@ echo -e "${GREEN}Configuring your development environment... ${NC}"
 
 # Git installation
 echo
-echo -e "1/11 ${LCYAN}Git installation... ${NC}"
+echo -e "1/10 ${LCYAN}Git installation... ${NC}"
 echo
 git init
 
 # ESLint & Prettier installation
 echo
-echo -e "2/11 ${LCYAN}ESLint & Prettier installation... ${NC}"
+echo -e "2/10 ${LCYAN}ESLint & Prettier installation... ${NC}"
 echo
 npm install -D eslint prettier
 
 # Airbnb's JavaScript style guide packages installation
 echo
-echo -e "3/11 ${LCYAN}Airbnb's JavaScript style guide packages installation... ${NC}"
+echo -e "3/10 ${LCYAN}Airbnb's JavaScript style guide packages installation... ${NC}"
 echo
 npm install -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react babel-eslint
 
 # Prettier's Eslint packages installation
 echo
-echo -e "4/11 ${LCYAN}Prettier's Eslint packages installation... ${NC}"
+echo -e "4/10 ${LCYAN}Prettier's Eslint packages installation... ${NC}"
 echo
 npm install -D eslint-config-prettier eslint-plugin-prettier
 
 # Building eslintrc.json file
 echo
-echo -e "5/11 ${LCYAN}Building eslintrc.json file...${NC}"
+echo -e "5/10 ${LCYAN}Building eslintrc.json file...${NC}"
 > ".eslintrc.json" # truncates existing file (or creates empty)
 
 echo '{
@@ -79,7 +79,7 @@ echo '{
 echo
 
 # Building prettierrc.json file
-echo -e "6/11 ${LCYAN}Building your prettierrc.json file... ${NC}"
+echo -e "6/10 ${LCYAN}Building your prettierrc.json file... ${NC}"
   > .prettierrc.json # truncates existing file (or creates empty)
 
   echo '{
@@ -93,7 +93,7 @@ echo
 # touch gulpfile.js
 
 # Building pack folders
-echo -e "8/11 ${LCYAN}Building pack folders and files... ${NC}"
+echo -e "8/10 ${LCYAN}Building pack folders and files... ${NC}"
 mkdir "css"
 mkdir "img"
 mkdir "js"
@@ -114,7 +114,7 @@ touch "scss/utilities/_variables.scss"
 echo
 
 # Building gitignore file for node_modules
-echo -e "9/11 ${LCYAN}Building gitignore file... ${NC}"
+echo -e "9/10 ${LCYAN}Building gitignore file... ${NC}"
   > .gitignore # truncates existing file (or creates empty)
   echo 'node_modules
 package-lock.json
@@ -124,7 +124,7 @@ package-lock.json
 echo
 
 # Building style.scss file
-echo -e "10/11 ${LCYAN}Building your style.scss file... ${NC}"
+echo -e "10/10 ${LCYAN}Building your style.scss file... ${NC}"
   > scss/style.scss # truncates existing file (or creates empty)
 
   echo '// ------------------------------ */
@@ -146,12 +146,6 @@ echo -e "10/11 ${LCYAN}Building your style.scss file... ${NC}"
     "layout/general/footer", "layout/general/displays";
 ' >> scss/style.scss
 echo
-
-# Removing script file
-echo
-echo -e "11/11 ${LCYAN}Removing the script file... ${NC}"
-echo
-rm frontend-starting-pack.sh
 
 # Finishing the script
 echo
